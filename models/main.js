4ca63b308d9838082fc550f840ca443b2940f165
ncaskey04 <ncaskey04@gmail.com>
2014-07-27 15:21:11 -0700

    all the methods work

--- models/main.js
+++ models/main.js
@@ -6,28 +6,31 @@ Models.Person = Person;
 var host = {firstname: 'Bob', lastname: 'Barker'};
 
 
-//Models.Person.create(host, function(err,person){
-//	console.log('Come on Down!');
-//});
+// Models.Person.create(host, function(err,person){
+// 	console.log('Come on Down!');
+// });
 
 
-
-//Models.Person.all(function(err, people){
+// Models.Person.all(function(err, people){
 //  console.log(people);
-//});
+// });
 
-Models.Person.findBy("id", 2, function(err, person){
- 	  console.log("found", person);
 
- 	  person.destroy(function(err,res){
- 		console.log('EXTERMINATE!', person);
-	 });
- // person.update({firstname: "sam", lastname: "creek"}, function(err, person){
- // console.log("UPDATED:", person)
+// Models.Person.findBy("id", 9, function(err, person){
+//  	console.log("found", person);
+//  	person.destroy(function(err,res){
+//  		console.log('EXTERMINATE!', person);
+// 	});
+// });
+
+
+Models.Person.findBy("id", 9, function(err, person){
+   console.log("found", person);
+    person.update({firstname: "sam", lastname: "creek"},
+    function(err, person){
+      console.log("UPDATED:", person);
+  });
 });
 
-// Models.Person.findBy('lastname','Barker', function(err,foundPerson){
-// 	console.log('Found: ', foundPerson);
-// });
 
 module.exports = Models;
